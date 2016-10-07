a4ae188c1a50e4c00be1766135f3db3fbccf785f
kaiba <luliruwy@163.com>
2016-10-07 10:38:13 +0800

    modify curator crud demo

--- demo/src/main/java/com/luliru/test/zk/curator/CrudExamples.java
+++ demo/src/main/java/com/luliru/test/zk/curator/CrudExamples.java
@@ -10,7 +10,9 @@ import org.apache.curator.framework.api.CuratorEvent;
 import org.apache.curator.framework.api.CuratorListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+
 import java.util.List;
 
 public class CrudExamples
@@ -22,19 +24,19 @@ public class CrudExamples
         create(client,"/curator/test","hello".getBytes());
     }
 
-    public static void      create(CuratorFramework client, String path, byte[] payload) throws Exception
+    public static void create(CuratorFramework client, String path, byte[] payload) throws Exception
     {
         // this will create the given ZNode with the given data
         client.create().forPath(path, payload);
     }
 
-    public static void      createEphemeral(CuratorFramework client, String path, byte[] payload) throws Exception
+    public static void createEphemeral(CuratorFramework client, String path, byte[] payload) throws Exception
     {
         // this will create the given EPHEMERAL ZNode with the given data
         client.create().withMode(CreateMode.EPHEMERAL).forPath(path, payload);
     }
 
-    public static String    createEphemeralSequential(CuratorFramework client, String path, byte[] payload) throws Exception
+    public static String createEphemeralSequential(CuratorFramework client, String path, byte[] payload) throws Exception
     {
         // this will create the given EPHEMERAL-SEQUENTIAL ZNode with the given data using Curator protection.
 
@@ -56,13 +58,13 @@ public class CrudExamples
         return client.create().withProtection().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath(path, payload);
     }
 
-    public static void      setData(CuratorFramework client, String path, byte[] payload) throws Exception
+    public static void setData(CuratorFramework client, String path, byte[] payload) throws Exception
     {
         // set data for the given node
         client.setData().forPath(path, payload);
     }
 
-    public static void      setDataAsync(CuratorFramework client, String path, byte[] payload) throws Exception
+    public static void setDataAsync(CuratorFramework client, String path, byte[] payload) throws Exception
     {
         // this is one method of getting event/async notifications
         CuratorListener listener = new CuratorListener()
@@ -80,19 +82,19 @@ public class CrudExamples
         client.setData().inBackground().forPath(path, payload);
     }
 
-    public static void      setDataAsyncWithCallback(CuratorFramework client, BackgroundCallback callback, String path, byte[] payload) throws Exception
+    public static void setDataAsyncWithCallback(CuratorFramework client, BackgroundCallback callback, String path, byte[] payload) throws Exception
     {
         // this is another method of getting notification of an async completion
         client.setData().inBackground(callback).forPath(path, payload);
     }
 
-    public static void      delete(CuratorFramework client, String path) throws Exception
+    public static void delete(CuratorFramework client, String path) throws Exception
     {
         // delete the given node
         client.delete().forPath(path);
     }
 
-    public static void      guaranteedDelete(CuratorFramework client, String path) throws Exception
+    public static void guaranteedDelete(CuratorFramework client, String path) throws Exception
     {
         // delete the given node and guarantee that it completes
 
@@ -113,7 +115,7 @@ public class CrudExamples
         client.delete().guaranteed().forPath(path);
     }
 
-    public static List<String> watchedGetChildren(CuratorFramework client, String path) throws Exception
+    public static List<String> defaultWatchedGetChildren(CuratorFramework client, String path) throws Exception
     {
         /**
          * Get children and set a watcher on the node. The watcher notification will come through the
@@ -122,11 +124,16 @@ public class CrudExamples
         return client.getChildren().watched().forPath(path);
     }
 
-    public static List<String> watchedGetChildren(CuratorFramework client, String path, Watcher watcher) throws Exception
+    public static List<String> watchedGetChildren(CuratorFramework client, String path) throws Exception
     {
         /**
          * Get children and set the given watcher on the node.
          */
-        return client.getChildren().usingWatcher(watcher).forPath(path);
+        return client.getChildren().usingWatcher(new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                System.out.println("node is changed");
+            }
+        }).forPath(path);
     }
 }
