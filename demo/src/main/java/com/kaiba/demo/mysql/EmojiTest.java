bac18b83cf6808f9a78b4b5ccf6144cbb7015014
luliru <lulr@gegejia.com>
2016-11-07 15:04:15 +0800

    add mysql emoji demo

--- demo/src/main/java/com/kaiba/demo/mysql/EmojiTest.java
+++ demo/src/main/java/com/kaiba/demo/mysql/EmojiTest.java
@@ -1,5 +1,6 @@
 package com.kaiba.demo.mysql;
 
+import java.io.UnsupportedEncodingException;
 import java.sql.*;
 
 /**
@@ -7,18 +8,20 @@ import java.sql.*;
  */
 public class EmojiTest {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        String str = "☺❤☎✉✌☝";
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, UnsupportedEncodingException {
+        String str = "\uD83D\uDC51\uD83D\uDD25";
+        byte[] bytes = str.getBytes("utf-8");
         System.out.println(str);
+        System.out.println(bytes.length);
 
         Class.forName("com.mysql.jdbc.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test?characterEncoding=UTF8","root","123456");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?characterEncoding=utf8","root","123456");
         PreparedStatement pstmt = conn.prepareStatement("insert into emoji(name) values(?)");
         pstmt.setString(1,str);
         pstmt.execute();
 
         pstmt = conn.prepareStatement("select name from emoji where id = ?");
-        pstmt.setInt(1,4);
+        pstmt.setInt(1,1);
         ResultSet rs = pstmt.executeQuery();
         while(rs.next()){
             System.out.println(rs.getString(1));
