0609902095519b8d5ef4d5b3c782affa18d114dc
luliru <lulr@gegejia.com>
2017-02-21 15:23:13 +0800

    add hystrix demo

---
+++ microserver/src/main/java/com/kaiba/microserver/hystrix/FallbackHelloWorldCommand.java
@@ -0,0 +1,37 @@
+package com.kaiba.microserver.hystrix;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by luliru on 2017/2/21.
+ */
+//重载HystrixCommand 的getFallback方法实现逻辑
+public class FallbackHelloWorldCommand extends HystrixCommand<String> {
+    private final String name;
+
+    public FallbackHelloWorldCommand(String name) {
+        super(HystrixCommand.Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("HelloWorldGroup"))
+                /* 配置依赖超时时间,500毫秒*/
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withExecutionTimeoutInMilliseconds(500)));
+        this.name = name;
+    }
+    @Override
+    protected String getFallback() {
+        return "exeucute Falled";
+    }
+    @Override
+    protected String run() throws Exception {
+        //sleep 1 秒,调用会超时
+        TimeUnit.MILLISECONDS.sleep(1000);
+        return "Hello " + name +" thread:" + Thread.currentThread().getName();
+    }
+    public static void main(String[] args) throws Exception{
+        FallbackHelloWorldCommand command = new FallbackHelloWorldCommand("test-Fallback");
+        String result = command.execute();
+        System.out.println(result);
+    }
+}
