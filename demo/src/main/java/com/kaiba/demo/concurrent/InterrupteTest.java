800e51ef17dfdc0728d32b5af518a8e3251d412d
luliru <lulr@gegejia.com>
2017-03-29 17:32:22 +0800

    modify thread interruput demo

--- demo/src/main/java/com/kaiba/demo/concurrent/InterrupteTest.java
+++ demo/src/main/java/com/kaiba/demo/concurrent/InterrupteTest.java
@@ -1,12 +1,17 @@
 package com.kaiba.demo.concurrent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by luliru on 2017/3/29.
  */
 public class InterrupteTest {
 
+    private final static Logger logger = LoggerFactory.getLogger(InterrupteTest.class);
+
     public static void main(String[] args) throws InterruptedException {
-        case_one();
+        case_5();
     }
 
     /**
@@ -55,5 +60,76 @@ public class InterrupteTest {
         };
         thread.start();
         System.out.println("Main thread over");
+    }
+
+    public static void case_three(){
+        Thread thread = new Thread(){
+
+            public void run(){
+                try {
+                    this.interrupt();
+                    System.out.println("Sub thread begin sleep");
+                    this.sleep(Long.MAX_VALUE);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+        System.out.println("Main thread over");
+    }
+
+    public static void case_4() throws InterruptedException {
+        Thread thread = new Thread(){
+
+            public void run(){
+                try {
+                    System.out.println("Sub thread begin sleep");
+                    this.sleep(Long.MAX_VALUE);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+        Thread.sleep(1000L);    //等待子线程wait
+        thread.interrupt();
+        System.out.println("Main thread over");
+    }
+
+    public static void case_5() throws InterruptedException {
+        final Object monitor = new Object();
+        Thread thread = new Thread(){
+            public void run(){
+                synchronized(monitor){
+                    while(!this.isInterrupted()){
+                        System.out.println("Sub thread do something...");
+                        System.out.println("Sub thread wait for shutdown.");
+                        try {
+                            monitor.wait(); //notify 不会抛出异常;interrupt()抛出异常
+                            System.out.println("Sub thread wake up");
+                            System.out.println("----"+this.isInterrupted());
+                            this.interrupt();
+                        } catch (InterruptedException e) {
+                            System.out.println("----"+this.isInterrupted());
+                            this.interrupt();
+                        }
+                    }
+                    System.out.println("Sub thread shutdown.");
+                }
+            }
+        };
+        thread.start();
+        System.out.println("Main thread do something...");
+        Thread.sleep(1000L);
+        System.out.println("Main thread begin shutdown sub thread.");
+        synchronized (monitor){
+            System.out.println("==="+thread.isInterrupted());
+//            monitor.notify();
+            thread.interrupt();
+            System.out.println("==="+thread.isInterrupted());
+            System.out.println("==="+thread.isInterrupted());
+        }
+        System.out.println("Main thread over.");
     }
 }
