c1a75960876a769644474c50ff7142e7ab5a8128
luliru <lulr@gegejia.com>
2017-03-29 14:54:56 +0800

    add LockSupport demo

---
+++ demo/src/main/java/com/kaiba/demo/concurrent/LockSupportTest.java
@@ -0,0 +1,27 @@
+package com.kaiba.demo.concurrent;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * Created by luliru on 2017/3/29.
+ */
+public class LockSupportTest {
+
+    public static void main(String[] args){
+        final Thread mainThread = Thread.currentThread();
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    Thread.sleep(3000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(this+" over.");
+                LockSupport.unpark(mainThread); //唤醒Main线程
+            }
+        };
+        thread.start();
+        LockSupport.park();     //休眠Main线程
+        System.out.println("Main thread over");
+    }
+}
