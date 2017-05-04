f7911807e87ed4b63a11b278ed0debc949b906ae
luliru <lulr@gegejia.com>
2017-05-04 15:10:33 +0800

    add reconnection prod demo

---
+++ demo/src/main/java/com/luliru/netty4/reconnectionprod/HeartBeatServerHandler.java
@@ -0,0 +1,38 @@
+package com.luliru.netty4.reconnectionprod;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+
+/**
+ * Created by luliru on 2017/5/3.
+ */
+public class HeartBeatServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleState state = ((IdleStateEvent) evt).state();
+            if (state == IdleState.READER_IDLE) {
+                ctx.channel().close();
+            }
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("server channelRead..");
+        System.out.println(ctx.channel().remoteAddress() + "->Server :" + msg.toString());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
