6575014c7e398e2a63191c7d8269ff1363bdb438
luliru <luliru@dianwoba.com>
2017-07-10 16:17:10 +0800

    perfect guava ratelimiter demo

--- demo/src/main/java/com/kaiba/demo/guava/RateLimiterDemo.java
+++ demo/src/main/java/com/kaiba/demo/guava/RateLimiterDemo.java
@@ -6,9 +6,10 @@ import com.google.common.util.concurrent.RateLimiter;
  * Created by luliru on 2017/2/24.
  */
 public class RateLimiterDemo {
-    public static void main(String[] args) {
-        testNoRateLimiter();
-        testWithRateLimiter();
+    public static void main(String[] args) throws InterruptedException {
+//        testNoRateLimiter();
+//        testWithRateLimiter();
+        testBursts();
     }
 
     public static void testNoRateLimiter() {
@@ -34,6 +35,19 @@ public class RateLimiterDemo {
 
         System.out.println(end - start);
 
+    }
+
+    public static void testBursts() throws InterruptedException {
+        RateLimiter limiter = RateLimiter.create(2.0);
+        Thread.sleep(1000L);
+        Long start = System.currentTimeMillis();
+        for (int i = 0; i < 4; i++) {
+            limiter.acquire(); // 请求RateLimiter, 超过permits会被阻塞
+            System.out.println("call execute.." + i);
+        }
+        Long end = System.currentTimeMillis();
+
+        System.out.println(end - start);
     }
 
 }
