127e752abb7ddde059b2ae2f1730a5e22de39cc4
luliru <luliru@dianwoba.com>
2017-09-18 15:14:39 +0800

    完善guava RateLimiter demo

--- demo/src/main/java/com/kaiba/demo/guava/ratelimiter/SmoothWarmingUpTest.java
+++ demo/src/main/java/com/kaiba/demo/guava/ratelimiter/SmoothWarmingUpTest.java
@@ -1,6 +1,8 @@
 package com.kaiba.demo.guava.ratelimiter;
 
 import com.google.common.util.concurrent.RateLimiter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,15 +11,32 @@ import java.util.concurrent.TimeUnit;
  */
 public class SmoothWarmingUpTest {
 
+    private static final Logger log = LoggerFactory.getLogger(SmoothWarmingUpTest.class);
+
     public static void main(String[] args) throws InterruptedException {
-        RateLimiter limiter = RateLimiter.create(5, 1000, TimeUnit.MILLISECONDS);
-        Thread.sleep(1000L);
-        for(int i = 1; i <= 5;i++) {
-            System.out.println(limiter.acquire());
+        RateLimiter limiter = RateLimiter.create(5, 2000, TimeUnit.MILLISECONDS);
+        Thread.sleep(3000L);
+        int index = 0;
+        long lastTime = System.currentTimeMillis();
+        for(int i = 1; i <= 20;i++) {
+            limiter.acquire();
+            index++;
+            long newTime = System.currentTimeMillis();
+            log.info(Integer.toString(index)+":"+Long.toString(newTime-lastTime));
+            lastTime = newTime;
         }
-        Thread.sleep(1000L);
-        for(int i = 1; i <= 5;i++) {
-            System.out.println(limiter.acquire());
+
+        log.info("==================");
+
+        Thread.sleep(3000L);
+        index = 0;
+        lastTime = System.currentTimeMillis();
+        for(int i = 1; i <= 10;i++) {
+            limiter.acquire();
+            index++;
+            long newTime = System.currentTimeMillis();
+            log.info(Integer.toString(index)+":"+Long.toString(newTime-lastTime));
+            lastTime = newTime;
         }
     }
 }
