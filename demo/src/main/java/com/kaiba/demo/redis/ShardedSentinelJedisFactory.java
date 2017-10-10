e11213edbdc621ff7e38115a2b68bda2be077d21
luliru <luliru@dianwoba.com>
2017-10-10 19:23:19 +0800

    RedisTemplate demo

---
+++ demo/src/main/java/com/kaiba/demo/redis/ShardedSentinelJedisFactory.java
@@ -0,0 +1,7 @@
+package com.kaiba.demo.redis;
+
+/**
+ * Created by luliru on 2017/9/29.
+ */
+public class ShardedSentinelJedisFactory {
+}
---
+++ demo/src/main/java/com/kaiba/demo/redis/spring/RedisTemplateDemo.java
@@ -0,0 +1,18 @@
+package com.kaiba.demo.redis.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * Created by luliru on 2017/10/10.
+ */
+public class RedisTemplateDemo {
+
+    public static void main(String[] args){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"classpath:redis/spring/redisTemplateDemo.xml"});
+        RedisTemplate template = ctx.getBean(RedisTemplate.class);
+        template.opsForValue().set("hello","world");
+        System.out.println(template.opsForValue().get("hello"));
+    }
+}
