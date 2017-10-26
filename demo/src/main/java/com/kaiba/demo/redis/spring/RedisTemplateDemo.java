cd23c5476cbb603edc3435396e8be69736261ead
luliru <luliru@dianwoba.com>
2017-10-26 20:52:36 +0800

    增加StringRedisTemplate配置

--- demo/src/main/java/com/kaiba/demo/redis/spring/RedisTemplateDemo.java
+++ demo/src/main/java/com/kaiba/demo/redis/spring/RedisTemplateDemo.java
@@ -3,6 +3,7 @@ package com.kaiba.demo.redis.spring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * Created by luliru on 2017/10/10.
@@ -11,8 +12,11 @@ public class RedisTemplateDemo {
 
     public static void main(String[] args){
         ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"classpath:redis/spring/redisTemplateDemo.xml"});
-        RedisTemplate template = ctx.getBean(RedisTemplate.class);
+        RedisTemplate template = ctx.getBean("redisTemplate",RedisTemplate.class);
         template.opsForValue().set("hello","world");
         System.out.println(template.opsForValue().get("hello"));
+
+        StringRedisTemplate stringRedisTemplate = ctx.getBean(StringRedisTemplate.class);
+        System.out.println(stringRedisTemplate.opsForValue().get("hello"));
     }
 }
