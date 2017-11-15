65105dfc471939f497a9dc0a4edaf90163bd999c
luliru <luliru@dianwoba.com>
2017-11-15 20:21:37 +0800

    调整zeromq demo

--- demo/src/main/java/com/kaiba/demo/zeromq/Request.java
+++ demo/src/main/java/com/kaiba/demo/zeromq/Request.java
@@ -1,5 +1,7 @@
 package com.kaiba.demo.zeromq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zeromq.ZMQ;
 
 /**
@@ -7,29 +9,18 @@ import org.zeromq.ZMQ;
  */
 public class Request {
 
+    private static final Logger log = LoggerFactory.getLogger(Request.class);
+
     public static void main(String args[]) {
-        for (int j = 0;  j < 5; j++) {
-            new Thread(new Runnable(){
+        ZMQ.Context context = ZMQ.context(1);  //创建一个I/O线程的上下文
+        ZMQ.Socket socket = context.socket(ZMQ.REQ);   //创建一个request类型的socket，这里可以将其简单的理解为客户端，用于向response端发送数据
 
-                public void run() {
-                    // TODO Auto-generated method stub
-                    ZMQ.Context context = ZMQ.context(1);  //创建一个I/O线程的上下文
-                    ZMQ.Socket socket = context.socket(ZMQ.REQ);   //创建一个request类型的socket，这里可以将其简单的理解为客户端，用于向response端发送数据
-
-                    socket.connect("tcp://127.0.0.1:5555");   //与response端建立连接
-                    long now = System.currentTimeMillis();
-                    for (int i = 0; i < 100000; i++) {
-                        String request = "hello";
-                        socket.send(request.getBytes());   //向reponse端发送数据
-                        byte[] response = socket.recv();   //接收response发送回来的数据  正在request/response模型中，send之后必须要recv之后才能继续send，这可能是为了保证整个request/response的流程走完
-                        //  System.out.println("receive : " + new String(response));
-                    }
-                    long after = System.currentTimeMillis();
-
-                    System.out.println((after - now) / 1000);
-                }
-
-            }).start();
+        socket.connect("tcp://127.0.0.1:5555");   //与response端建立连接
+        for (int i = 0; i < 20; i++) {
+            socket.send(Integer.toString(i).getBytes());   //向reponse端发送数据
+            log.info("send : " + i);
+            byte[] response = socket.recv();   //接收response发送回来的数据  正在request/response模型中，send之后必须要recv之后才能继续send，这可能是为了保证整个request/response的流程走完
+            log.info("receive : " + new String(response));
         }
     }
 }
