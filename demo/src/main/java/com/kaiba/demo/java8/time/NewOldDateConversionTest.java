b936b9ed92b83d631bb8bc8095b8fcd1915f24ab
luliru <luliru@dianwoba.com>
2018-03-28 00:05:02 +0800

    update java8 time demo

--- demo/src/main/java/com/kaiba/demo/java8/time/NewOldDateConversionTest.java
+++ demo/src/main/java/com/kaiba/demo/java8/time/NewOldDateConversionTest.java
@@ -1,8 +1,6 @@
 package com.kaiba.demo.java8.time;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.*;
 import java.util.Date;
 
 /**
@@ -10,11 +8,27 @@ import java.util.Date;
  */
 public class NewOldDateConversionTest {
 
+    public static void main(String[] args){
+        LocalDateToDate();
+    }
+
     // 04. java.time.LocalDateTime --> java.util.Date
-    public void LocalDateTimeToUdate() {
+    public static void LocalDateTimeToUdate() {
         LocalDateTime localDateTime = LocalDateTime.now();
         ZoneId zone = ZoneId.systemDefault();
         Instant instant = localDateTime.atZone(zone).toInstant();
         java.util.Date date = Date.from(instant);
+    }
+
+    // java.time.LocalDate --> java.util.Date
+    public static void LocalDateToDate(){
+        ZoneId zoneId = ZoneId.systemDefault();
+        LocalDate localDate = LocalDate.now();
+        ZonedDateTime zdt = localDate.atStartOfDay(zoneId);
+
+        Date date = Date.from(zdt.toInstant());
+
+        System.out.println("LocalDate = " + localDate);
+        System.out.println("Date = " + date);
     }
 }
