24b2ed2f0e893929a745e4efbb413b7548b45c60
luliru <luliru@dianwoba.com>
2018-07-16 20:15:18 +0800

    create gradle-demo

---
+++ gradle-demo/src/main/java/com/luliru/practice/gradle/demo/Main.java
@@ -0,0 +1,13 @@
+package com.luliru.practice.gradle.demo;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * Created by luliru on 2018/7/16.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println(StringUtils.isNotBlank("hello"));
+    }
+}
